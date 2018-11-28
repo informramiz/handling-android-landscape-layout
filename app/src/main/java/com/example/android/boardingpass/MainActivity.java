9613d7bff7c61523058ee3ec803e447361823e0d
Ramiz Raja <informramiz@gmail.com>
2018-11-28 09:34:23 +0500

    refactor: Extract flight info UI as separate layout

--- app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -51,9 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
         mBinding.textViewPassengerName.setText(info.passengerName);
         // TODO (7) Use the flightInfor attribute in mBinding below to get the appropiate text Views
-        mBinding.textViewOriginAirport.setText(info.originCode);
-        mBinding.textViewFlightCode.setText(info.flightCode);
-        mBinding.textViewDestinationAirport.setText(info.destCode);
+        mBinding.flightInfo.textViewOriginAirport.setText(info.originCode);
+        mBinding.flightInfo.textViewFlightCode.setText(info.flightCode);
+        mBinding.flightInfo.textViewDestinationAirport.setText(info.destCode);
 
         SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault());
         String boardingTime = formatter.format(info.boardingTime);
